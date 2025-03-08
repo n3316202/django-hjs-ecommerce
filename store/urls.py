df4ac7e262488082097e36e2a38c19c129a6b1af
heo <admin@admin.com>
2025-03-09 01:29:48 +0900

    commit

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,9 @@ app_name = "store"
 urlpatterns = [
     path("", views.home, name="home"),
     path("/about", views.about, name="about"),
-    path("product/<int:product_id>", views.product, name="product"), #dev_12
+    path("product/<int:product_id>", views.product, name="product"),  # dev_12
+    path(
+        "category/<str:foo>", views.category, name="category"
+    ),  # 변수 foo 의 의미  참고:https://namu.wiki/w/foo #dev_13
+    path('category_summary/', views.category_summary, name='category_summary'),
 ]

730fbd53ef9bd36a113aa7afdd15f1c7a4fd0362
heo <admin@admin.com>
2025-03-08 20:46:56 +0900

    commit

--- store/models.py
+++ store/models.py
@@ -13,10 +13,16 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)  # 99999999999.99
+    price = models.DecimalField(
+        default=0, decimal_places=2, max_digits=10
+    )  # 99999999999.99
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default="", blank=True, null=True)
     image = models.ImageField(upload_to="upload/product")
+    # 판매 가능 여부(Add Sale stuff)
+    # dev_6
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name

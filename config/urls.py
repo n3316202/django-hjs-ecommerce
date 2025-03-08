091d75b150ded2bcf7e90b88cb67918de537892f
heo <admin@admin.com>
2025-03-08 16:11:40 +0900

    commit

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
+from config import settings
 from store import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
 ]
+# dev_2
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

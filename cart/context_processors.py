1944920f9df023bdbe5c99fd2304bd50294028d2
heo <admin@admin.com>
2025-03-09 14:59:52 +0900

    commit

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -3,5 +3,6 @@ from .cart import Cart  # 현재폴더 cart.py 에서 Cart객체를 가져옴
 
 # dev_14
 def cart(request):
-    print(request)
+    print("카트 컨텍스트")
+    print(request.session)
     return {"cart": Cart(request)}

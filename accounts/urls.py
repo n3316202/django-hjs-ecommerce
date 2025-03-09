be19f51c6b662dcfb62002eb610ea4c528bfc36c
heo <admin@admin.com>
2025-03-09 18:24:37 +0900

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,13 +8,22 @@ from accounts import views
 # dev_7
 app_name = "accounts"
 
+# dev_22
+# urlpatterns = [
+#     # django.contrib.auth앱의 LoginView 클래스를 활용했으므로 별도의 views.py 파일 수정이 필요 없음
+#     path(
+#         "login/",
+#         auth_views.LoginView.as_view(template_name="accounts/login.html"),
+#         name="login",
+#     ),
+#     path("logout/", views.log_out, name="logout"),  # 코드 추가하기
+#     path("signup/", views.sign_up, name="signup"),
+# ]
+
 urlpatterns = [
     # django.contrib.auth앱의 LoginView 클래스를 활용했으므로 별도의 views.py 파일 수정이 필요 없음
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
-        name="login",
-    ),
-    path("logout/", views.log_out, name="logout"),  # 코드 추가하기
-    path("signup/", views.sign_up, name="signup"), 
+     #path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'), 
+     path('login/', views.login_user, name='login'), 
+     path('logout/', views.logout_user, name='logout'),
+     path('register/', views.register_user, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,29 +2,19 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
+from cart.cart import Cart
+from cart.models import CartItem
+from django.contrib import messages
 
-# dev_7
+
+# dev_22
 # Create your views here.
-def log_out(request):
+def logout_user(request):
     logout(request)
     return redirect("/")
 
 
-def sign_up(request):
-    # if request.method == "POST":
-    #     form = UserForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         username = form.cleaned_data.get("username")
-    #         raw_password = form.cleaned_data.get("password1")
-    #         user = authenticate(username=username, password=raw_password)  # 사용자 인증
-    #         login(request, user)  # 로그인
-    #         return redirect("/")
-    # else:
-    #     form = UserForm()
-
-    # return render(request, "accounts/signup.html", {"form": form})
-
+def register_user(request):
     if request.method == "POST":
         if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(
@@ -36,3 +26,35 @@ def sign_up(request):
             return redirect("/")
         return render(request, "signup.html")
     return render(request, "accounts/signup.html")
+
+
+# dev_22
+def login_user(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            saved_cart_item = CartItem.objects.filter(cart__user__id=request.user.id)
+
+            if saved_cart_item:
+
+                # Get the cart
+                cart = Cart(request)
+
+                for item in saved_cart_item:
+                    print("===========", item.product.id)
+                    print("===========", item.quantity)
+                    cart.add(product=item.product, quantity=item.quantity)
+
+            messages.success(request, "You Have been logged in")
+            return redirect("/")
+        else:
+            messages.success(request, ("There was an error, please try again"))
+            return redirect("login")
+    else:
+        return render(request, "accounts/login.html", {})
